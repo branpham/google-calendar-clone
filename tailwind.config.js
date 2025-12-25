7568f3f3eb667b39484752b50b893fcea6fa620a
Brandon Pham <33362481+branpham@users.noreply.github.com>
2025-12-24 20:32:02 -0500

    Add Tailwind CSS configuration with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'cal-bg': '#202124',
+        'cal-border': '#3c4043',
+        'cal-text': '#e8eaed',
+      }
+    }
+  },
+  darkMode: 'class',
+  plugins: []
+}
